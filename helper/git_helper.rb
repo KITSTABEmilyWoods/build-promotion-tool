187c5fb0c6d8c0714f7936bac368389d527a1267
Emily Woods <emily.woods@theappbusiness.com>
2017-01-30 12:02:49 +0000

    Add method for pushing tag to remote

--- helper/git_helper.rb
+++ helper/git_helper.rb
@@ -1,27 +1,26 @@
 class GitHelper
 
-  def all_tags
-    `git tag -l`.lines
+  def fetch_tags
+    `git fetch --tags`
   end
 
-  def apply_tag(tag)
-    `git tag #{tag}`
+  def all_tags
+    `git tag -l`.lines
   end
 
   def get_tags_for_this_commit
     `git tag --points-at HEAD`.lines
   end
 
-  def fetch_tags
-    `git fetch --tags`
+  def apply_tag(tag)
+    `git tag #{tag}`
   end
 
-  def push_tag_to_remote
-    `git push origin HEAD`
+  def push_tags_to_remote(tag)
+    `git push origin #{tag}`
   end
-
 
 end
 
-git_helper = GitHelper.new
-git_helper.push_tag_to_remote
+g = GitHelper.new
+g.push_tags_to_remote('dev-v4.4.4')
