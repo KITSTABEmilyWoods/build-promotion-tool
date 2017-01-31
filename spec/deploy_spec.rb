fa5df4903abd8a30a0e31e99aa4a8d15f0a63761
Emily Woods <emily.woods@theappbusiness.com>
2017-01-31 17:51:21 +0000

    Add additional tests for user_comms and for deploy controller

--- spec/deploy_spec.rb
+++ spec/deploy_spec.rb
@@ -3,61 +3,224 @@ require_relative '../controller/deploy_controller.rb'
 
 describe 'DeployController' do
 
-  before(:each) do
-    @develop_tag_generator = spy('develop_tag_generator')
-    @git_helper = spy('git_helper')
-    @user_comms = spy('user_comms')
-    @test_tag_generator = spy('test_tag_generator')
-    @deploy = DeployController.new('develop', @git_helper, @user_comms, @develop_tag_generator, @test_tag_generator)
-  end
+  let(:develop_tag_generator) {spy('develop_tag_generator')}
+  let(:git_helper) {spy('git_helper')}
+  let(:user_comms) {spy('user_comms')}
+  let(:other_tag_generator) {spy('other_tag_generator')}
 
   describe 'environment_choice' do
+
+    subject(:deploy) {DeployController.new('develop', git_helper, user_comms, develop_tag_generator, other_tag_generator)}
+
     before(:each) do
-      allow(@git_helper).to receive(:get_tags_for_this_commit)
+      allow(git_helper).to receive(:get_tags_for_this_commit)
     end
+
     describe 'develop' do
       context 'when the user request to apply a new develop tag with patch increment'
       before(:each) do
+        @dev_tag = 'dev-v0.1.2'
+        allow(other_tag_generator).to receive(:tag_exists?).and_return(false)
+        allow(develop_tag_generator).to receive(:develop_tag_exists).and_return(true)
 
-
-        allow(@test_tag_generator).to receive(:check_for_tag?).and_return(false)
-        allow(@develop_tag_generator).to receive(:develop_tag_exists).and_return(true)
-
-        allow(@user_comms).to receive(:ask_increment_type)
-        allow(@user_comms).to receive(:user_increment_choice).and_return('patch')
-        allow(@develop_tag_generator).to receive(:next_develop_tag).with('patch').and_return('dev-v0.1.2')
-        allow(@user_comms).to receive(:ask_permissison_to_apply).with('dev-v0.1.2')
+        allow(user_comms).to receive(:ask_increment_type)
+        allow(user_comms).to receive(:user_increment_choice).and_return('patch')
+        allow(develop_tag_generator).to receive(:next_develop_tag).and_return(@dev_tag)
+        allow(user_comms).to receive(:ask_permissison_to_apply)
       end
 
       context 'when they say yes to applying the tag' do
         before(:each) do
-          allow(@user_comms).to receive(:user_reply_y_or_n).and_return('y')
+          allow(user_comms).to receive(:user_reply_y_or_n).and_return('y')
+        end
+
+        it "asks the user's perimission to apply tag dev-v0.1.2" do
+          deploy.environment_choice
+          expect(user_comms).to have_received(:ask_permissison_to_apply).with(@dev_tag)
         end
 
         it 'receives the next tag to apply' do
-          @deploy.environment_choice
-          expect(@git_helper).to have_received(:apply_tag).with('dev-v0.1.2')
+          deploy.environment_choice
+          expect(git_helper).to have_received(:apply_tag).with(@dev_tag)
+        end
+
+        it 'pushes next tag to the remote' do
+          deploy.environment_choice
+          expect(git_helper).to have_received(:push_tag_to_remote).with(@dev_tag)
         end
 
         it "says 'thank you' to the user" do
-          @deploy.environment_choice
-          expect(@user_comms).to have_received(:say_thank_you)
+          deploy.environment_choice
+          expect(user_comms).to have_received(:say_thank_you)
         end
       end
 
       context 'when they say no to applying the tag' do
         before(:each) do
-          allow(@user_comms).to receive(:user_reply_y_or_n).and_return('n')
+          allow(user_comms).to receive(:user_reply_y_or_n).and_return('n')
         end
 
         it'says that no tag has been applied' do
-          @deploy.environment_choice
-          expect(@user_comms).to have_received(:say_no_tag_applied)
+          deploy.environment_choice
+          expect(user_comms).to have_received(:say_no_tag_applied)
         end
 
         it "says 'thank you' to the user" do
-          @deploy.environment_choice
-          expect(@user_comms).to have_received(:say_thank_you)
+          deploy.environment_choice
+          expect(user_comms).to have_received(:say_thank_you)
+        end
+      end
+
+      context 'when there is already a develop tag on the commit'do
+        before(:each) do
+          allow(other_tag_generator).to receive(:tag_exists?).and_return(true)
+        end
+
+        it "tells the user that there is already at dev tag on the commit" do
+          deploy.environment_choice
+          expect(user_comms).to have_received(:error_commit_has_dev_tag)
+        end
+      end
+
+        context 'when there are no tags on the repository'do
+          before(:each) do
+            @dev_tag = 'dev-v0.0.1'
+            allow(other_tag_generator).to receive(:tag_exists?).and_return(false)
+            allow(develop_tag_generator).to receive(:develop_tag_exists?).and_return(false)
+
+            allow(user_comms).to receive(:tell_user_no_develop_tags)
+            allow(develop_tag_generator).to receive(:first_tag).and_return(@dev_tag)
+
+            allow(user_comms).to receive(:ask_permissison_to_apply)
+            allow(user_comms).to receive(:user_reply_y_or_n).and_return('y')
+          end
+
+          it "asks the user's permission to apply the first tag" do
+            deploy.environment_choice
+            expect(user_comms).to have_received(:ask_permissison_to_apply).with(@dev_tag)
+          end
+
+          it "applies the first tag" do
+            deploy.environment_choice
+            expect(git_helper).to have_received(:apply_tag).with(@dev_tag)
+          end
+
+          it "pushes the first tag" do
+            deploy.environment_choice
+            expect(git_helper).to have_received(:push_tag_to_remote).with(@dev_tag)
+          end
+        end
+      end
+
+      describe 'test' do
+        context ' when the user would like to apply a test tag'do
+
+        subject(:deploy) {DeployController.new('test', git_helper, user_comms, develop_tag_generator, other_tag_generator)}
+
+        context 'when there is a develop tag, dev-v0.1.5 on the commit' do
+          before(:each) do
+            @tags_for_this_commit = ['dev-v0.1.1', 'dev-v0.2.0']
+            @test_tag = 'test-v0.1.5'
+
+            allow(git_helper).to receive(:get_tags_for_this_commit).and_return(@tags_for_this_commit)
+
+            allow(other_tag_generator).to receive(:tag_exists?).with('dev', @tags_for_this_commit).and_return(true)
+            allow(other_tag_generator).to receive(:tag_exists?).with('test', @tags_for_this_commit).and_return(false)
+
+            allow(other_tag_generator).to receive(:next_tag).and_return(@test_tag)
+
+            allow(user_comms).to receive(:ask_permissison_to_apply)
+            allow(user_comms).to receive(:user_reply_y_or_n).and_return('y')
+          end
+
+          context 'when the user says yes to applying the next tag' do
+            before(:each) do
+              allow(user_comms).to receive(:user_reply_y_or_n).and_return('y')
+            end
+
+            it 'asks permission to apply test tag, test-v0.1.5' do
+              deploy.environment_choice
+              expect(user_comms).to have_received(:ask_permissison_to_apply).with(@test_tag)
+            end
+
+            it 'applies a test tag, test-v0.1.5' do
+              deploy.environment_choice
+              expect(git_helper).to have_received(:push_tag_to_remote).with(@test_tag)
+            end
+          end
+
+          context 'when the user says no to applying the next tag' do
+            before(:each) do
+              allow(user_comms).to receive(:user_reply_y_or_n).and_return('n')
+            end
+
+            it 'asks permission to apply test tag, test-v0.1.5' do
+              deploy.environment_choice
+              expect(user_comms).to have_received(:ask_permissison_to_apply).with(@test_tag)
+            end
+
+            it 'says no tag has been applied' do
+              deploy.environment_choice
+              expect(user_comms).to have_received(:say_no_tag_applied)
+            end
+          end
+        end
+
+
+        describe 'stage' do
+          context ' when the user would like to apply a stage tag'do
+
+            subject(:deploy) {DeployController.new('stage', git_helper, user_comms, develop_tag_generator, other_tag_generator)}
+
+            context 'when there is a test tag, test-v0.1.5 on the commit' do
+              before(:each) do
+                @tags_for_this_commit = ['dev-v0.1.1', 'test-v0.2.0']
+                @stage_tag = 'test-v0.2.0'
+
+                allow(git_helper).to receive(:get_tags_for_this_commit).and_return(@tags_for_this_commit)
+
+                allow(other_tag_generator).to receive(:tag_exists?).with('test', @tags_for_this_commit).and_return(true)
+                allow(other_tag_generator).to receive(:tag_exists?).with('stage', @tags_for_this_commit).and_return(false)
+
+                allow(other_tag_generator).to receive(:next_tag).and_return(@stage_tag)
+
+                allow(user_comms).to receive(:ask_permissison_to_apply)
+              end
+
+              context 'when the user says yes to applying the next tag' do
+                before(:each) do
+                  allow(user_comms).to receive(:user_reply_y_or_n).and_return('y')
+                end
+
+                it 'asks permission to apply test tag, stage-v0.1.5' do
+                  deploy.environment_choice
+                  expect(user_comms).to have_received(:ask_permissison_to_apply).with(@stage_tag)
+                end
+
+                it 'applies a stage tag, stage-v0.1.5' do
+                  deploy.environment_choice
+                  expect(git_helper).to have_received(:push_tag_to_remote).with(@stage_tag)
+                end
+              end
+
+
+              context 'when the user says no to applying the next tag' do
+                before(:each) do
+                  allow(user_comms).to receive(:user_reply_y_or_n).and_return('n')
+                end
+
+                it 'asks permission to apply stage tag, test-v0.1.5' do
+                  deploy.environment_choice
+                  expect(user_comms).to have_received(:ask_permissison_to_apply).with(@stage_tag)
+                end
+
+                it 'says no tag has been applied' do
+                  deploy.environment_choice
+                  expect(user_comms).to have_received(:say_no_tag_applied)
+                end
+              end
+            end
+          end
         end
       end
     end
--- spec/other_tag_spec.rb
+++ spec/other_tag_spec.rb
@@ -1,55 +1,53 @@
 require 'rspec'
-require_relative '../generator/test_and_stage_tag_generator'
+require_relative '../generator/other_tag_generator'
 
-describe 'TestAndStageTagGenerator' do
-  before(:each) do
-    @test_tag_generator = TestAndStageTagGenerator.new
-  end
+describe 'OtherTagGenerator' do
+  subject(:other_tag_generator) { OtherTagGenerator.new }
 
-  describe 'check_for_tag?' do
+  describe 'tag_exists?' do
     before(:each) do
       @check_tag_type = 'dev'
     end
 
     context 'when there are no develop tags on the commit but there are other tags' do
       it 'says there are no develop tag' do
-        expect(@test_tag_generator.check_for_tag?(@check_tag_type, ['test-v1.1.1', 'test-v1.2.1'])).to eql(false)
+        expect(other_tag_generator.tag_exists?(@check_tag_type, ['test-v1.1.1', 'test-v1.2.1'])).to eql(false)
       end
     end
 
     context 'when there are no tags on the commit' do
       it 'says there are no develop tag' do
-        expect(@test_tag_generator.check_for_tag?(@check_tag_type, [])).to eql(false)
+        expect(other_tag_generator.tag_exists?(@check_tag_type, [])).to eql(false)
       end
     end
 
     context 'when there is a develop tag on the commit' do
       it 'says there is a develop tag' do
-        expect(@test_tag_generator.check_for_tag?(@check_tag_type, ['dev-v0.1.1'])).to eql(true)
+        expect(other_tag_generator.tag_exists?(@check_tag_type, ['dev-v0.1.1'])).to eql(true)
       end
     end
 
     context 'when there is both a develop and test tag on the commit' do
       it 'says there is a develop tag' do
-        expect(@test_tag_generator.check_for_tag?(@check_tag_type, ['dev-v0.1.1', 'test-v0.1.1'])).to eql(true)
+        expect(other_tag_generator.tag_exists?(@check_tag_type, ['dev-v0.1.1', 'test-v0.1.1'])).to eql(true)
       end
     end
 
     context 'when there is a tag kind of like deveop' do
       it 'says there is a develop tag' do
-        expect(@test_tag_generator.check_for_tag?(@check_tag_type, ['dev-v0.1.1adljalal'])).to eql(false)
+        expect(other_tag_generator.tag_exists?(@check_tag_type, ['dev-v0.1.1adljalal'])).to eql(false)
       end
     end
 
     context 'when there is a different tag type' do
       it 'says there is a develop tag' do
-        expect(@test_tag_generator.check_for_tag?(@check_tag_type, ['hello'])).to eql(false)
+        expect(other_tag_generator.tag_exists?(@check_tag_type, ['hello'])).to eql(false)
       end
     end
 
     context 'when there are more than one dev tags' do
       it 'says there is a develop tag' do
-        expect(@test_tag_generator.check_for_tag?(@check_tag_type, ['dev-v0.1.1', 'dev-v0.1.2'])).to eql(true)
+        expect(other_tag_generator.tag_exists?(@check_tag_type, ['dev-v0.1.1', 'dev-v0.1.2'])).to eql(true)
       end
     end
 
@@ -59,7 +57,7 @@ describe 'TestAndStageTagGenerator' do
       end
 
       it 'says there is a test tag' do
-        expect(@test_tag_generator.check_for_tag?(@check_tag_type, ['test-v0.1.1'])).to eql(true)
+        expect(other_tag_generator.tag_exists?(@check_tag_type, ['test-v0.1.1'])).to eql(true)
       end
     end
   end
@@ -72,13 +70,13 @@ describe 'TestAndStageTagGenerator' do
 
     context 'when there is a develop tag: dev-v0.1.1' do
       it 'returns a test tag: test-v0.1.1' do
-        expect(@test_tag_generator.next_tag(@previous_tag_type, @next_tag_type, ['dev-v0.1.1'])).to eql('test-v0.1.1')
+        expect(other_tag_generator.next_tag(@previous_tag_type, @next_tag_type, ['dev-v0.1.1'])).to eql('test-v0.1.1')
       end
     end
 
     context 'when there are more than one dev tags' do
       it 'returns the first' do
-        expect(@test_tag_generator.next_tag(@previous_tag_type, @next_tag_type,['dev-v0.1.1', 'dev-v0.1.2', 'dev-v0.2.1'])).to eql('test-v0.1.1')
+        expect(other_tag_generator.next_tag(@previous_tag_type, @next_tag_type,['dev-v0.1.1', 'dev-v0.1.2', 'dev-v0.2.1'])).to eql('test-v0.1.1')
       end
     end
 
